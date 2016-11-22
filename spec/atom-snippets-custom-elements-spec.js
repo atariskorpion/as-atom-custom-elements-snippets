b13e77af0d227065ccd175ebf19e1616ebc0a132
AtariSkorpion <atriskorpion@local-MacBook.local>
2016-11-22 19:18:42 +0100

    Rename travis yml file.

--- spec/atom-snippets-custom-elements-spec.js
+++ spec/atom-snippets-custom-elements-spec.js
@@ -4,7 +4,6 @@
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
-
 describe('AtomSnippetsCustomElements', () => {
   let activationPromise;
 
